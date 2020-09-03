1f467b985ef25063092c8903711957e53e95c2af
dimdbr <ddd260501@gmail.com>
2020-09-03 16:53:40 +0200

    initial commit

---
+++ shape_example/src/com/company/Shape.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Shape {
+    private final Color color;
+    public Shape(Color color) {this.color = color;}
+    public Color getColor() { return this.color;}
+    public double getArea() {return Double.NaN;}
+
+}
